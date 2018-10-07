ec59210b85019e2039504980affecd737529ac56
Arthur Colombini Gusmão <arthurcgusmao@hotmail.com>
2018-10-07 03:00:09 -0300

    Refactor & Improve report text
    In this commit we improved the report text and refactored images and the
    markdown file to the `report` directory. We have set up the markdown
    file to support PDF generation via Pandoc and other filters, which we
    will use when submitting the project.

--- model.py
+++ model.py
@@ -6,10 +6,10 @@ from sklearn.metrics import roc_auc_score
 
 
 class Model(object):
-    
+
     def __init__(self, model_def, window_size=None):
         """Arguments:
-        
+
         - `model_def`: can be either a model definition (see `model_defs.py` file) or a string corresponding to a path to an exported model.
         """
         if type(model_def) == str:
@@ -18,15 +18,15 @@ class Model(object):
             self.tf_model_path = os.path.join(self.results_dir, 'tf-model')
             self.model_info_filepath = os.path.join(self.results_dir, 'model_info.tsv')
             self.lock_training = True
-            
+
             self.model_info = pd.read_csv(self.model_info_filepath, sep='\t').loc[0].to_dict()
             self.window_size = self.model_info['window_size']
-            
+
             tf.reset_default_graph()
             self.sess = tf.Session()
             new_saver = tf.train.import_meta_graph(self.tf_model_path + '.meta', clear_devices=True)
             new_saver.restore(self.sess, tf.train.latest_checkpoint(self.results_dir))
-            
+
             graph = tf.get_default_graph()
             self.inputs = graph.get_tensor_by_name('inputs:0')
             self.preds = graph.get_tensor_by_name('predictions:0')
@@ -48,7 +48,7 @@ class Model(object):
             tf.identity(self.logits, name="out_logits_1")
 
             self.labels = tf.placeholder(tf.float32, [None,  6], name='labels')
-            self.preds = tf.nn.sigmoid(self.logits, name='predictions')      
+            self.preds = tf.nn.sigmoid(self.logits, name='predictions')
             self.cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=self.logits, labels=self.labels))
             print('Model initizalized.')
 
@@ -63,32 +63,32 @@ class Model(object):
             }
             self.tf_model_path = os.path.join(self.results_dir, 'tf-model')
             self.lock_training = False
-        
+
     def set_data(self, train, valid):
         self.train = train
         self.valid = valid
         self.model_info['# train'] = len(train)
         self.model_info['# valid'] = len(valid)
-        
+
     def fit(self, epochs, batch_size, batches_gen, lr=0.001):
         if self.lock_training:
             print('Training locked!')
             return
         self.lock_training = True
-        
+
         print('Starting learning...')
         with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
             self.optimizer = tf.train.AdamOptimizer(learning_rate=lr).minimize(self.cost)
         self.saver = tf.train.Saver()
         self.sess = tf.Session()
         self.sess.run(tf.global_variables_initializer())
-        
+
         start_time = time.time()
         learning_curve = []
         self.model_info['batch_size'] = batch_size
         self.model_info['lr'] = lr
         pd.DataFrame([self.model_info]).to_csv(self.model_info_filepath, sep='\t') # save model info in results dir
-        
+
         # loop through epochs
         for e in range(epochs):
             loss = 0
@@ -102,7 +102,7 @@ class Model(object):
                 loss += res # calculate loss
                 n_total_batches += 1
                 partial_loss += res; partial_n_batches_count += 1 # calculate partial loss
-                
+
                 if n_total_batches % print_partial_loss_every == 0:
                     loss_print = partial_loss/partial_n_batches_count # get mean loss to make loss comparable among different batch sizes
                     print("Epoch: {},\tBatch: {},\tMean Loss (last batches): {}".format(e, n_total_batches, loss_print))
@@ -116,7 +116,7 @@ class Model(object):
 #                     print("Validating...")
 #                     valid_mean_auc = self.validate(batch_size, batches_gen)
 #                     print("Valid Mean AUC: {}".format(valid_mean_auc))
-            
+
             # print train and valid information
             loss /= n_total_batches # get mean loss to make loss comparable among different batch sizes
             valid_mean_auc = self.validate(batch_size, batches_gen)
@@ -132,10 +132,10 @@ class Model(object):
 
         train_time = time.time() - start_time
         print("Model was trained in {} seconds.".format(train_time))
-        
-        
-        
-    def validate(self, batch_size, batches_gen):
+
+
+
+    def validate(self, batch_size, batches_gen, average='macro'):
         """Calculates the mean ROC AUC."""
         total_predictions = []
         total_labels = pd.DataFrame()
@@ -143,9 +143,9 @@ class Model(object):
             predictions = self.sess.run(self.preds, feed_dict={self.inputs: xx, self.is_training: False})
             total_predictions.extend(predictions)
             total_labels = pd.concat((total_labels, yy))
-        mean_auc = roc_auc_score(total_labels, total_predictions, average='macro')
+        mean_auc = roc_auc_score(total_labels, total_predictions, average=average)
         return mean_auc
-    
+
     def predict(self, data, batch_size, batches_gen):
         """Return the predictions for a data series."""
         total_predictions = []
@@ -153,8 +153,8 @@ class Model(object):
             predictions = self.sess.run(self.preds, feed_dict={self.inputs: xx, self.is_training: False})
             total_predictions.extend(predictions)
         return total_predictions
-    
-    
+
+
     def save_model(self):
         """Exports a model to disk"""
         self.saver.save(self.sess, self.tf_model_path)
