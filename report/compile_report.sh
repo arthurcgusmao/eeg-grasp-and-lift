fc42912b63d03ef50da4c266eba6f2d5875b6ac0
Arthur Colombini Gusmão <arthurcgusmao@hotmail.com>
2018-10-09 22:51:14 -0300

    Compile Report command
    A bash command was saved into a file in order to keep history of
    pandoc filters used.

---
+++ report/compile_report.sh
@@ -0,0 +1,1 @@
+pandoc --filter=/home/arthurcgusmao/.miniconda3/bin/pandoc-fignos --filter=/home/arthurcgusmao/.miniconda3/bin/pandoc-eqnos --filter=pandoc-citeproc capstone_report.md -o capstone_report.pdf
